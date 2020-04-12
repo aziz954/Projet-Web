d7be5a2362dc1d6c1bec1007f3e4df8af1b85fc6
aziz954 <55796207+aziz954@users.noreply.github.com>
2020-04-12 15:17:47 +0100

    Add files via upload

---
+++ Aziz_Allouche CRUD V0/PHP_CRUD_Operation-master/php/db.php
@@ -0,0 +1,41 @@
+<?php
+
+function Createdb(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "medline";
+
+    // create connection
+    $con = mysqli_connect($servername, $username, $password);
+
+    // Check Connection
+    if (!$con){
+        die("Connection Failed : " . mysqli_connect_error());
+    }
+
+    // create Database
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+    if(mysqli_query($con, $sql)){
+        $con = mysqli_connect($servername, $username, $password, $dbname);
+
+        $sql = "
+                        CREATE TABLE IF NOT EXISTS com(
+                            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            idc INT(11) NOT NULL,
+                            q float(11) NOT NULL
+                        );
+        ";
+
+        if(mysqli_query($con, $sql)){
+            return $con;
+        }else{
+            echo "Cannot Create table...!";
+        }
+
+    }else{
+        echo "Error while creating database ". mysqli_error($con);
+    }
+
+}
